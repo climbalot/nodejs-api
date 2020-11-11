3ae4ea51851dcc12152421dd5e444259289c51cb
Florence Loy <florencelry@gmail.com>
2020-11-11 18:09:51 +0800

    edited dockerfile and start script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,4 @@
 FROM ubuntu:18.04
-MAINTAINER uli.hitzel@gmail.com
 EXPOSE 8080
 ARG DEBIAN_FRONTEND=noninteractive
 ENV TZ=Asia/Singapore
@@ -9,6 +8,7 @@ RUN apt-get install -y nodejs npm
 ENV USER root
 RUN npm install -g express-generator
 RUN npm install express --save
+RUN npm install cors --save
 RUN useradd -ms /bin/bash user
 COPY app.js /home/user/app.js
 COPY start.sh /home/user/start.sh
---
+++ server.js
@@ -0,0 +1,30 @@
+const bodyParser = require("body-parser"); // import body-parser package which is used to parse the body's content from the request
+const express = require("express");
+const cors = require("cors");
+
+// import routers
+const router = express.Router;
+
+// URI mapping to display all users
+router.get("/users", (request, response) => {
+    // in the callback function, use mysql connection to execute select query
+    connection.query(
+        `select user_id, name, email, mobile from users`,
+        (errors, results) => {
+        // populate the response object with the results received from mysql server.
+            response.send(results);
+        }
+    );
+});
+
+// create an instance of express which will start the server.
+application = express();
+application.use(cors());
+application.use(bodyParser.json()); // use body parser to specify how to convert body's content.
+
+// start the application on port 8080
+application.listen(8080, (error) => {
+  if (!error) {
+    console.log("Application started succesfully");
+  }
+});
